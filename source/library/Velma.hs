dfc034e760160c71eac3c890a0e092f853443ff2
Taylor Fausak <taylor@fausak.me>
2022-02-12 17:28:04 -0500

    Add documentation to all exported identifiers

--- source/library/Velma.hs
+++ source/library/Velma.hs
@@ -22,12 +22,24 @@ import qualified System.Directory as Directory
 import qualified System.FilePath as FilePath
 import qualified Velma.SymbolicPath as SymbolicPath
 
+-- | The default entrypoint for this custom setup script. This calls Cabal's
+-- 'Cabal.defaultMainWithHooks' with our custom 'userHooks'.
+--
+-- If you're trying to use Velma in your own project, you should create a
+-- @Setup.hs@ file like this:
+--
+-- > -- Setup.hs
+-- > import Velma
+-- > main = defaultMain
 defaultMain :: IO ()
 defaultMain = Cabal.defaultMainWithHooks userHooks
 
+-- | Like Cabal's 'Cabal.simpleUserHooks' but with our custom 'confHook'.
 userHooks :: Cabal.UserHooks
 userHooks = Cabal.simpleUserHooks { Cabal.confHook = confHook }
 
+-- | Calls 'discover' before handing things off to the 'Cabal.confHook' from
+-- Cabal's 'Cabal.simpleUserHooks'.
 confHook
     :: (Cabal.GenericPackageDescription, HookedBuildInfo.HookedBuildInfo)
     -> Cabal.ConfigFlags
@@ -36,13 +48,18 @@ confHook (gpd1, hbi) cf = do
     gpd2 <- discover gpd1
     Cabal.confHook Cabal.simpleUserHooks (gpd2, hbi) cf
 
+-- | Simply calls 'discoverWith' with 'listDirectoryRecursively'.
 discover
     :: Cabal.GenericPackageDescription -> IO Cabal.GenericPackageDescription
 discover = discoverWith listDirectoryRecursively
 
+-- | Discovers modules in all of the components of this package description.
+-- You can think of this function as calling 'discoverComponent' for each
+-- component: library, sub-libraries, foreign libraries, executables, test
+-- suites, and benchmarks.
 discoverWith
     :: Monad m
-    => (FilePath -> m [FilePath])
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> Cabal.GenericPackageDescription
     -> m Cabal.GenericPackageDescription
 discoverWith f = concatM
@@ -64,15 +81,30 @@ discoverWith f = concatM
         (mapM . overF (Lens._2 . condTreeData) $ discoverBenchmark f)
     ]
 
+-- | Applies all of the functions left-to-right using '(Monad.>=>)'.
+--
+-- >>> let printAnd f x = do { putStrLn $ "x = " <> show x; pure $ f x }
+-- >>> concatM [ printAnd (+ 2), printAnd (* 2) ] 3
+-- x = 3
+-- x = 5
+-- 10
 concatM :: Monad m => [a -> m a] -> a -> m a
 concatM = foldr (Monad.>=>) pure
 
+-- | Like 'Lens.over' except the modification function can perform arbitrary
+-- effects.
+--
+-- >>> overF _2 (Just . (+ 2)) ('a', 3)
+-- Just ('a',5)
+-- >>> overF _2 (const Nothing) ('a', 3)
+-- Nothing
 overF :: Functor f => Lens.Lens' s a -> (a -> f a) -> s -> f s
 overF l f x = (\y -> Lens.set l y x) <$> f (Lens.view l x)
 
+-- | Thin wrapper around 'discoverComponent' for libraries.
 discoverLibrary
     :: Monad m
-    => (FilePath -> m [FilePath])
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> Cabal.Library
     -> m Cabal.Library
 discoverLibrary f = concatM
@@ -80,16 +112,18 @@ discoverLibrary f = concatM
     , discoverComponent Cabal.otherModules (Lens.view Cabal.exposedModules) f
     ]
 
+-- | Thin wrapper around 'discoverComponent' for foreign libraries.
 discoverForeignLib
-    :: Monad m
-    => (FilePath -> m [FilePath])
+    :: Applicative m
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> Cabal.ForeignLib
     -> m Cabal.ForeignLib
 discoverForeignLib = discoverComponent Cabal.otherModules $ const []
 
+-- | Thin wrapper around 'discoverComponent' for executables.
 discoverExecutable
-    :: Monad m
-    => (FilePath -> m [FilePath])
+    :: Applicative m
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> Cabal.Executable
     -> m Cabal.Executable
 discoverExecutable =
@@ -98,9 +132,10 @@ discoverExecutable =
         . filePathToModuleName
         . Lens.view Cabal.modulePath
 
+-- | Thin wrapper around 'discoverComponent' for test suites.
 discoverTestSuite
-    :: Monad m
-    => (FilePath -> m [FilePath])
+    :: Applicative m
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> Cabal.TestSuite
     -> m Cabal.TestSuite
 discoverTestSuite = discoverComponent Cabal.otherModules $ \ts ->
@@ -110,9 +145,10 @@ discoverTestSuite = discoverComponent Cabal.otherModules $ \ts ->
         TestSuiteInterface.TestSuiteLibV09 _ mn -> [mn]
         TestSuiteInterface.TestSuiteUnsupported _ -> []
 
+-- | Thin wrapper around 'discoverComponent' for benchmarks.
 discoverBenchmark
-    :: Monad m
-    => (FilePath -> m [FilePath])
+    :: Applicative m
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> Cabal.Benchmark
     -> m Cabal.Benchmark
 discoverBenchmark = discoverComponent Cabal.otherModules $ \ts ->
@@ -121,11 +157,18 @@ discoverBenchmark = discoverComponent Cabal.otherModules $ \ts ->
             Maybe.maybeToList $ filePathToModuleName fp
         BenchmarkInterface.BenchmarkUnsupported _ -> []
 
+-- | Discovers modules in the given component, using the provided lens to
+-- select which field to update. This is the main workhorse of the package.
 discoverComponent
     :: (Cabal.HasBuildInfo a, Applicative m)
     => Lens.Lens' a [ModuleName.ModuleName]
+    -- ^ Typically something like 'Cabal.exposedModules'.
     -> (a -> [ModuleName.ModuleName])
-    -> (FilePath -> m [FilePath])
+    -- ^ This function is used to get a list of module names to avoid
+    -- discovering. For example if you're populating 'Cabal.exposedModules',
+    -- then you'll want to use 'Cabal.otherModules' here to avoid discovering
+    -- duplicates.
+    -> (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> a
     -> m a
 discoverComponent includeL toExclude f component =
@@ -145,9 +188,12 @@ discoverComponent includeL toExclude f component =
                     component
             in addDiscovered <$> getModuleNames f component
 
+-- | Gets module names for the given component, using the provided function to
+-- list directory contents. This basically just glues together
+-- 'getHsSourceDirs', 'listDirectoryRecursively', and 'filePathToModuleName'.
 getModuleNames
     :: (Cabal.HasBuildInfo a, Applicative m)
-    => (FilePath -> m [FilePath])
+    => (FilePath -> m [FilePath]) -- ^ See 'listDirectoryRecursively'.
     -> a
     -> m (Set.Set ModuleName.ModuleName)
 getModuleNames f =
@@ -155,11 +201,19 @@ getModuleNames f =
         . traverse (\d -> fmap (FilePath.makeRelative d) <$> f d)
         . getHsSourceDirs
 
+-- | A lens for the 'CondTree.condTreeData' field.
 condTreeData :: Lens.Lens' (CondTree.CondTree v c a) a
 condTreeData f ct =
     fmap (\d -> ct { CondTree.condTreeData = d }) . f $ CondTree.condTreeData
         ct
 
+-- | Gets @hs-source-dirs@ from the given component.
+--
+-- - If @hs-source-dirs@ isn't set (or is empty), this will return the inferred
+-- directory, which is the current directory (@"."@).
+-- - Duplicates are removed from the result using 'ListUtils.nubOrd'.
+-- - This should probably return @SymbolicPath@ values, but that type was only
+-- introduced in recent versions (>= 3.6) of Cabal.
 getHsSourceDirs :: Cabal.HasBuildInfo a => a -> [FilePath]
 getHsSourceDirs =
     ListUtils.nubOrd
@@ -167,22 +221,64 @@ getHsSourceDirs =
         . fmap SymbolicPath.toFilePath
         . Lens.view Cabal.hsSourceDirs
 
+-- | Attempts to remove an element from the list. If it succeeds, returns the
+-- list without that element. If it fails, returns 'Nothing'.
+--
+-- >>> maybeRemove 'b' "abc"
+-- Just "ac"
+-- >>> maybeRemove 'z' "abc"
+-- Nothing
+--
+-- Note that only the first matching element is removed.
+--
+-- >>> maybeRemove 'b' "abcb"
+-- Just "acb"
 maybeRemove :: Eq a => a -> [a] -> Maybe [a]
 maybeRemove x ys = case ys of
     [] -> Nothing
     y : zs -> if x == y then Just zs else (:) y <$> maybeRemove x zs
 
+-- | The magic module name that enables automatic module discovery with Velma.
+-- This will always be @Velma.Discover@.
 velmaDiscoverModuleName :: ModuleName.ModuleName
 velmaDiscoverModuleName = ModuleName.fromString "Velma.Discover"
 
+-- | Attempts to convert a 'FilePath' into a 'ModuleName.ModuleName'. This
+-- works by stripping certain extensions, then converting directory separators
+-- into module separators, and finally trying to parse that as a module name.
+--
+-- >>> filePathToModuleName "Velma.hs"
+-- Just (ModuleName "Velma")
+-- >>> filePathToModuleName "Velma/SymbolicPath.hs"
+-- Just (ModuleName "Velma.SymbolicPath")
+-- >>> filePathToModuleName "README.markdown"
+-- Nothing
+-- >>> filePathToModuleName "library/Velma.hs"
+-- Nothing
 filePathToModuleName :: FilePath -> Maybe ModuleName.ModuleName
 filePathToModuleName filePath = do
     base <- FilePath.stripExtension "hs" filePath
     Parsec.simpleParsec . List.intercalate "." $ FilePath.splitDirectories base
 
-withDefault :: Foldable t => t a -> t a -> t a
+-- | Returns the given default value if the other value is 'null'. For example:
+--
+-- >>> withDefault ["default"] []
+-- ["default"]
+-- >>> withDefault ["default"] ["something"]
+-- ["something"]
+withDefault
+    :: Foldable t
+    => t a -- ^ The default value.
+    -> t a
+    -> t a
 withDefault d x = if null x then d else x
 
+-- | Lists all of the directory contents recursively. The returned file paths
+-- will include the directory prefix, unlike 'Directory.listDirectory'. For
+-- example:
+--
+-- >>> listDirectoryRecursively "source/library"
+-- ["source/library/Velma.hs","source/library/Velma/SymbolicPath.hs"]
 listDirectoryRecursively :: FilePath -> IO [FilePath]
 listDirectoryRecursively directory = do
     let
